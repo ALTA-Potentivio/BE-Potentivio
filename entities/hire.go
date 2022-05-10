e95fdcb0b00bf4200f6b060c54bf8db807021df1
abbiyudha <abbiyudha14@gmail.com>
2022-05-10 12:34:07 +0700

    add payment url to return

--- entities/hire.go
+++ entities/hire.go
@@ -19,7 +19,7 @@ type Hire struct {
 	Price               float64   `gorm:"not null" json:"price" form:"price"`
 	AccountNumberArtist *string   `gorm:"not null" json:"account_number_artist" form:"account_number_artist"`
 	AccountNumberCafe   *string   `gorm:"not null" json:"account_number_cafe" form:"account_number_cafe"`
-	PaymentUrl          *string   `json:"payment_url" form:"payment_url"`
+	PaymentUrl          string    `json:"payment_url" form:"payment_url"`
 	IDXendit            string    `json:"id_xendit"`
 	Artist              Artist    `gorm:"foreignKey:IdArtist;references:ID"`
 	Cafe                Cafe      `gorm:"foreignKey:IdCafe;references:ID"`
